e570cba00f87b6e00a143dbb9dcf43c7d78d49cd
Ruben Murga <ruben.murgat@gmail.com>
2023-04-23 13:31:09 -0600

    adding [/otherfiles] to clarify project

--- otherfiles/db-scripts.sql
+++ otherfiles/db-scripts.sql
@@ -20,9 +20,7 @@
 	    unique (id)
 	);
 
-
 	SELECT * FROM stocklist
-
 
 	INSERT stocklist(name, http_web) VALUES ('Blim', 'https://www-stage.blim.com/inicio')
 
@@ -30,10 +28,7 @@
 	UPDATE movies SET stocklist_id = 2 WHERE  id in (2,4)
 	UPDATE movies SET stocklist_id = 3 WHERE  id in (6)
 
-
 	INSERT INTO movies(name, description, image, stocklist_id) VALUES ('The Whale', 'n a town in Idaho, Charlie, a reclusive and unhealthy English teacher, hides out in his flat and eats his way to death.', 'https://flim-1-0-2.s3.eu-central-1.amazonaws.com/thumbs/hd_720px/982c682cdbed11ea84df10ddb1aba44f.jpeg',4)
 	INSERT INTO movies(name, description, image, stocklist_id) VALUES ('Flash Gordon', 'n a town in Idaho, Charlie, a reclusive and unhealthy English teacher, hides out in his flat and eats his way to death.', 'https://www.thescarboroughnews.co.uk/webimg/QVNIMTE2NjU2MzMy.jpg?width=1200&enable=upscale',5)
 	INSERT INTO movies(name, description, image, stocklist_id) VALUES ('Total Recall', 'TOTAL RECALL represents one of the iconic composer’s last masterpieces, recorded with the National Philharmonic Orchestra of London, under his own baton.', 'https://m.media-amazon.com/images/M/MV5BYzU1YmJjMGEtMjY4Yy00MTFlLWE3NTUtNzI3YjkwZTMxZjZmXkEyXkFqcGdeQXVyNDc2NjEyMw@@._V1_FMjpg_UX1000_.jpg',5)
-
-
 
